a1209a022a660e03907875b6b0e344e20a4c0a2c
mindnbytes <mindnbytes@gmail.com>
2026-08-15 18:09:14 +0200

    Configure Git URL rewrite for GitHub ADemyanchuk repos

--- nixos/programs/git.nix
+++ nixos/programs/git.nix
@@ -9,6 +9,7 @@
         username = "mindnbytes";
         email = "mindnbytes@gmail.com";
       };
+      url."git@github-ad.com:ademyanchuk/".insteadOf = "git@github.com:ademyanchuk/";
       init.defaultBranch = "master";
       color.ui = "auto";
       push.autoSetupRemote = true;
