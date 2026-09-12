cd08b34402e446659ee69c80593b543a1254bdfb
mindnbytes <mindnbytes@gmail.com>
2026-09-12 09:32:26 +0200

    Add persistent Quake terminal panel for Niri

--- nixos/home/alex/desktops/niri/default.nix
+++ nixos/home/alex/desktops/niri/default.nix
@@ -1,6 +1,14 @@
 { osConfig, pkgs, ... }:
 
 {
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "quake-toggle";
+      runtimeInputs = [ osConfig.programs.niri.package pkgs.ghostty pkgs.jq ];
+      text = builtins.readFile ./scripts/quake-toggle;
+    })
+  ];
+
   xdg.configFile."niri/config.kdl".source =
     pkgs.runCommand "niri-config"
       {
