e23080981af75a0075d8534d4c298a0e30bda5fe
mindnbytes <mindnbytes@gmail.com>
2026-09-14 17:39:21 +0200

    Moved to nixpkgs maintained opencode v1 package

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -14,11 +14,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-
-    opencode-v2 = {
-      url = "github:/anomalyco/opencode/beta";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
--- nixos/home/alex/default.nix
+++ nixos/home/alex/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -24,7 +24,7 @@
       gh
       keepassxc
 
-      inputs.opencode-v2.packages.${pkgs.stdenv.hostPlatform.system}.opencode
+      opencode
     ];
   };
   programs.home-manager.enable = true;
